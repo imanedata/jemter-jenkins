25e0c128dfccacf35125e431072d602e755abd84
Imane <belkessa.imanebelkes@gmail.com>
2025-04-02 14:10:50 +0200

    first commit

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    WebDriver driver;
+
+    @Test
+    public void test1(){
+      
+    }
+}
